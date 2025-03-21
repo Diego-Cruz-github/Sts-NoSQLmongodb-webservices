37cbefd99e893be76f32aecfbb8a0f028d8bbbf6
Diego Fonte Boa <diegofonteboacruz@gmail.com>
2025-03-21 15:47:30 -0300

    criação do projeto

---
+++ src/main/java/com/diegofonteboa/StsNoSQLmongodbwebservices/StsNoSqLmongodbwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.diegofonteboa.StsNoSQLmongodbwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsNoSqLmongodbwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsNoSqLmongodbwebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegofonteboa/StsNoSQLmongodbwebservices/StsNoSqLmongodbwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegofonteboa.StsNoSQLmongodbwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsNoSqLmongodbwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
