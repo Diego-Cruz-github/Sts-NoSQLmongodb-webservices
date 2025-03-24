30ee880f9c7483a037a887e96101a98894ccb1e7
Diego Fonte Boa <diegofonteboacruz@gmail.com>
2025-03-23 22:09:18 -0300

    Obtendo um usuário por id e exceptions

--- src/main/java/com/diegofonteboa/StsNoSQLmongodbwebservices/services/UserService.java
+++ src/main/java/com/diegofonteboa/StsNoSQLmongodbwebservices/services/UserService.java
@@ -1,20 +1,29 @@
 package com.diegofonteboa.StsNoSQLmongodbwebservices.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diegofonteboa.StsNoSQLmongodbwebservices.domain.User;
 import com.diegofonteboa.StsNoSQLmongodbwebservices.repository.UserRepository;
+import com.diegofonteboa.StsNoSQLmongodbwebservices.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
 }
