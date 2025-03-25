fd60d114ef1434466b7ae6856e7e88738013f741
Diego Fonte Boa <diegofonteboacruz@gmail.com>
2025-03-25 18:03:31 -0300

    projecao dos dados do autor com DTO

--- src/main/java/com/diegofonteboa/StsNoSQLmongodbwebservices/config/Instanciation.java
+++ src/main/java/com/diegofonteboa/StsNoSQLmongodbwebservices/config/Instanciation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.diegofonteboa.StsNoSQLmongodbwebservices.domain.Post;
 import com.diegofonteboa.StsNoSQLmongodbwebservices.domain.User;
+import com.diegofonteboa.StsNoSQLmongodbwebservices.dto.AuthorDTO;
 import com.diegofonteboa.StsNoSQLmongodbwebservices.repository.PostRepository;
 import com.diegofonteboa.StsNoSQLmongodbwebservices.repository.UserRepository;
 
@@ -37,8 +38,8 @@ public class Instanciation implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO (maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2)); // Corrigido para saveAll()
     }
