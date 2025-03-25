2631696ce1008bf34316d9e44ef60356331938d1
Diego Fonte Boa <diegofonteboacruz@gmail.com>
2025-03-25 18:26:58 -0300

    Obtendo um post por id

---
+++ src/main/java/com/diegofonteboa/StsNoSQLmongodbwebservices/services/PostService.java
@@ -0,0 +1,23 @@
+package com.diegofonteboa.StsNoSQLmongodbwebservices.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegofonteboa.StsNoSQLmongodbwebservices.domain.Post;
+import com.diegofonteboa.StsNoSQLmongodbwebservices.repository.PostRepository;
+import com.diegofonteboa.StsNoSQLmongodbwebservices.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
+	
