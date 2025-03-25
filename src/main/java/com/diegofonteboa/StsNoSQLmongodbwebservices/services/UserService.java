6850e112ee5e9d5d282b3d5508f3664614046de9
Diego Fonte Boa <diegofonteboacruz@gmail.com>
2025-03-25 14:47:36 -0300

    insercao usuarios com o post

--- src/main/java/com/diegofonteboa/StsNoSQLmongodbwebservices/services/UserService.java
+++ src/main/java/com/diegofonteboa/StsNoSQLmongodbwebservices/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diegofonteboa.StsNoSQLmongodbwebservices.domain.User;
+import com.diegofonteboa.StsNoSQLmongodbwebservices.dto.UserDTO;
 import com.diegofonteboa.StsNoSQLmongodbwebservices.repository.UserRepository;
 import com.diegofonteboa.StsNoSQLmongodbwebservices.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,18 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User insert1 (User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 	
