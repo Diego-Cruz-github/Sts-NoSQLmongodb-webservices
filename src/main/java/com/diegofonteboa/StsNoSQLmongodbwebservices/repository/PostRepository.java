cb4ddd98914ec2e1aa705f289c20f83080b52855
Diego Fonte Boa <diegofonteboacruz@gmail.com>
2025-03-25 21:38:40 -0300

    Consulta simples com @Query

--- src/main/java/com/diegofonteboa/StsNoSQLmongodbwebservices/repository/PostRepository.java
+++ src/main/java/com/diegofonteboa/StsNoSQLmongodbwebservices/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.diegofonteboa.StsNoSQLmongodbwebservices.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.diegofonteboa.StsNoSQLmongodbwebservices.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("\"{ 'title': { $regex: ?0, $options: 'i' } }\"") //Json //? 0 = 1 parametro
+	List<Post> searchTitle (String text); //parametro usado
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
